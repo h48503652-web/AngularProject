e7122096d6e25bae255a60a3ade530428563d7b2
h48503652-web <h48503652@gmail.com>
2026-01-30 02:33:11 +0200

    Update API URL for production

--- myFronted/src/environments/environment.prod.ts
+++ myFronted/src/environments/environment.prod.ts
@@ -1,5 +1,5 @@
 export const environment = {
   production: true,
   // Replace with your deployed API URL
-  apiBaseUrl: 'https://<your-service>.onrender.com/api'
+  apiBaseUrl: 'https://nexus-flow-server.onrender.com'
 };
